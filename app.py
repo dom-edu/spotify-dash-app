0f50727272bbd269a85f049953bea1071e5d3760
dom-edu <84454639+dom-edu@users.noreply.github.com>
2025-12-19 16:13:25 -0500

    added app layout

--- app.py
+++ app.py
@@ -3,10 +3,14 @@ import pandas as pd
 import plotly.express as px
 
 
-app = Dash(__name__, title="Spotify App")
-
 # DATA 
 spotify_df = pd.read_csv("data/spotify_data_clean.csv")
+
+app = Dash(__name__, title="Spotify App")
+
+app.layout = [
+    
+]
 
 if __name__ == '__main__':
     app.run(debug=True)
